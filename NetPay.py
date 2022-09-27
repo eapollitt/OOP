2219859be832ab38214586bb4f06da5babd1cd02
Elizabeth Anne Pollitt <elizabethanne_polli1@baylor.edu>
2022-09-27 12:38:45 -0500

    saves

---
+++ NetPay.py
@@ -0,0 +1,7 @@
+#one employee and five instances from the payroll deduction class
+#comparing to see if the employee ids match jimmy smith and deduct that from his total pay monthly salary and if id doesnt then dont deduct it
+#use a for loop to go through th ids 
+#hard code the values in the instance side 
+#class all accessor methods (get methods )
+#calculations can be done in the program side 
+# 
